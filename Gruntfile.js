a7d9870dd9c679bfebe6e14feee80473919a4a2c
Mihai Peteu <jericho1ne@yahoo.com>
2016-01-24 23:24:15 -0800

    two words -- screwIE8: true

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,324 +8,372 @@
 //       'test/spec/**/*.js'
 
 module.exports = function (grunt) {
-    // Time how long tasks take. Can help when optimizing build times
-    // require('time-grunt')(grunt);
+	// Time how long tasks take. Can help when optimizing build times
+	// require('time-grunt')(grunt);
 
-    // Automatically load required grunt tasks
-    // require('jit-grunt')(grunt, {
-    //   useminPrepare: 'grunt-usemin'
-    // });
+	// Automatically load required grunt tasks
+	// require('jit-grunt')(grunt, {
+	//   useminPrepare: 'grunt-usemin'
+	// });
 
-    // Configurable paths
-    var config = {
-        app: 'src',
-        dist: 'dist'
-    };
+	// cfgurable paths
+	var cfg = {
+		app: 'src',
+		dst: 'dist'
+	};
 
-    // Define the configuration for all the tasks
-    grunt.initConfig({
+	// Define the cfguration for all the tasks
+	grunt.initConfig({
 
-      // Project settings
-      config: config,
+		// Project settings
+		cfg: cfg,
 
-      // Empties folders to start fresh
-      clean: {
-         dist: {
-            files: [{
-               dot: true,
-               src: [
-                  '.tmp',
-                  '<%= config.dist %>/*',
-                  '!<%= config.dist %>/.git*'
-               ]
-            }]
-         },
-         server: '.tmp'
-      },
+		// Empties folders to start fresh
+		clean: {
+			dist: {
+				files: [{
+					dot: true,
+					src: [
+						'.tmp',
+						'<%= cfg.dst %>/*',
+						'!<%= cfg.dst %>/.git*'
+					]
+				}]
+			},
+			server: '.tmp'
+		},
 
-      // Runs JSHint (code quality analysis tool) against app JS files
-      // Rules defined in .jshintrc.
-      // THROWS ERRORS!!
-      //jshint: {
-      // options: {
-      //    jshintrc: '.jshintrc',
-      //    reporter: require('jshint-stylish')
-      // },
-      // assess: ['<%=config.app%>/js/**/*.js'],
-      // all: ['Gruntfile.js', '<%=config.app%>/js/**/*.js']
-      //},
-
-      // Watches files for changes and runs tasks based on what's changed
-      //watch: require('./grunt-include/watch'),
-      //browserSync: require('./grunt-include/browserSync'),
-
-      // Mocha testing framework configuration options
-      mocha: {
-         all: {
-            options: {
-               run: true,
-               urls: ['http://<%= browserSync.test.options.host %>:<%= browserSync.test.options.port %>/index.html']
-            }
-         }
-      },
-   
-      // // Compiles Sass to CSS and generates necessary files if requested
-      // sass: {
-      //    options: {
-      //       sourceMap: true,
-      //       sourceMapEmbed: true,
-      //       sourceMapContents: true,
-      //       includePaths: ['.']
-      //    },
-      //    dist: {
-      //       files: [{
-      //          expand: true,
-      //          cwd: '<%= config.app %>/styles',
-      //          src: ['*.{scss,sass}'],
-      //          dest: '.tmp/styles',
-      //          ext: '.css'
-      //       }]
-      //    }
-      // },
+		// Mocha testing framework cfguration options
+		mocha: {
+			all: {
+				options: {
+					run: true,
+					urls: ['http://<%= browserSync.test.options.host %>:<%= browserSync.test.options.port %>/index.html']
+				}
+			 }
+		},
+	   
+		// // Compiles Sass to CSS and generates necessary files if requested
+		// sass: {
+		//    options: {
+		//       sourceMap: true,
+		//       sourceMapEmbed: true,
+		//       sourceMapContents: true,
+		//       includePaths: ['.']
+		//    },
+		//    dist: {
+		//       files: [{
+		//          expand: true,
+		//          cwd: '<%= cfg.app %>/styles',
+		//          src: ['*.{scss,sass}'],
+		//          dest: '.tmp/styles',
+		//          ext: '.css'
+		//       }]
+		//    }
+		// },
 
 
-      /**
-       * These plugins can lint your CSS, support variables and mixins, 
-       * transpile future CSS syntax, inline images, etc
-       *
-       * https://github.com/postcss/postcss
-       */
-      postcss: {
-         options: {
-            map: true,
-            processors: [
-                // Add vendor prefixed styles
-                require('autoprefixer')({
-                    browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']
-                })
-            ]
-         },
-         dist: {
-            files: [{
-               expand: true,
-               cwd: '.tmp/styles/',
-               src: '{,*/}*.css',
-               dest: '.tmp/styles/'
-            }]
-         }
-      },
+		/**
+		* These plugins can lint your CSS, support variables and mixins, 
+		* transpile future CSS syntax, inline images, etc
+		*
+		* https://github.com/postcss/postcss
+		*/
+		postcss: {
+			options: {
+				map: true,
+				processors: [
+					// Add vendor prefixed styles
+					require('autoprefixer')({
+						browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']
+					})
+				]
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '.tmp/styles/',
+					src: '{,*/}*.css',
+					dest: '.tmp/styles/'
+				}]
+			}
+		},
 
 
-      // All the bower crap
-      // wiredep: require('./grunt-include/wiredep'),
+		// All the bower crap
+		// wiredep: require('./grunt-include/wiredep'),
 
-      // Renames files for browser caching purposes
-      filerev: {
-         dist: {
-            src: [
-               '<%= config.dist %>/scripts/{,*/}*.js',
-               '<%= config.dist %>/styles/{,*/}*.css',
-               '<%= config.dist %>/images/{,*/}*.*',
-               '<%= config.dist %>/styles/fonts/{,*/}*.*',
-               '<%= config.dist %>/*.{ico,png}'
-            ]
-         }
-      },
+		// Renames files for browser caching purposes
+		filerev: {
+			dist: {
+				src: [
+					'<%= cfg.dst %>/scripts/{,*/}*.js',
+					'<%= cfg.dst %>/styles/{,*/}*.css',
+					'<%= cfg.dst %>/images/{,*/}*.*',
+					'<%= cfg.dst %>/styles/fonts/{,*/}*.*',
+					'<%= cfg.dst %>/*.{ico,png}'
+				]
+			}
+		},
 
-      // // Reads HTML for usemin blocks to enable smart builds that automatically
-      // // concat, minify and revision files. Creates configurations in memory so
-      // // additional tasks can operate on them
-      // useminPrepare: {
-      //    options: {
-      //       dest: '<%= config.dist %>'
-      //    },
-      //    html: '<%= config.app %>/index.html'
-      // },
+		// By default, your `index.html`'s <!-- Usemin block --> will take care
+		// of minification. These next options are pre-cfgured if you do not
+		// wish to use the Usemin blocks.
+		cssmin: {
+			dist: {
+				files: {
+					'<%= cfg.dst %>/styles/main.css': [
+						'.tmp/styles/{,*/}*.css',
+						'<%= cfg.app %>/styles/{,*/}*.css'
+					]
+				}
+			}
+		},
 
-      // // Performs rewrites based on rev and the useminPrepare configuration
-      // usemin: {
-      //    options: {
-      //       assetsDirs: [
-      //          '<%= config.dist %>',
-      //          '<%= config.dist %>/images',
-      //          '<%= config.dist %>/styles'
-      //       ]
-      //    },
-      //    html: ['<%= config.dist %>/{,*/}*.html'],
-      //    css: ['<%= config.dist %>/styles/{,*/}*.css']
-      // },
+		/**
+		 * Uglify: minify js files
+		 * Needs to happen after JS file get copied to dist
+		 * https://github.com/gruntjs/grunt-contrib-uglify
+		 */
+		uglify: {
+			options: {
+				preserveComments: false,
+				screwIE8: true,
+				mangle: false,
+				cwd: '<%= cfg.app %>/scripts/vendor/'
+				// exceptionsFiles: [
+				// 	'datatables.min.js', 
+				// 	'jquery-2.2.0.min.js'
+				// ]
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%= cfg.app %>/scripts/vendor/',
+					src: '*.js',
+					dest: '<%= cfg.dst %>/scripts/vendor',
+					ext:  '.min.js'
+				}]
+				// files: {
+				// 	// This does not remove the original source files 
+				// 	// { 
+				// 	// 	src: 'src/scripts/vendor/jquery-ui.js', 
+				// 	// 	dest: 'dist/scripts/vendor/jquery-ui.min.js'
+				// 	// }
+				// 	'<%= cfg.dst %>/scripts/vendor/jquery-ui.min.js':['<%= cfg.dst %>/scripts/vendor/jquery-ui.js'],
+				// 	'<%= cfg.dst %>/scripts/vendor/modernizr.min.js':['<%= cfg.dst %>/scripts/vendor/modernizr.js']
+				// }
+			}// End uglify:dist
+		},// End uglify task
 
-      // // The following *-min tasks produce minified files in the dist folder
-      // imagemin: {
-      //    dist: {
-      //       files: [{
-      //          expand: true,
-      //          cwd: '<%= config.app %>/images',
-      //          src: '{,*/}*.{gif,jpeg,jpg,png}',
-      //          dest: '<%= config.dist %>/images'
-      //       }]
-      //    }
-      // },
+		// Copies remaining files to places other tasks can use
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					dot: true,
+					cwd: '<%= cfg.app %>',
+					dest: '<%= cfg.dst %>',
+					src: [
+						'{,*/}*.html',
+						'scripts/**/*.{js,php}',
+						// 'scripts/**/*.php',
+						'*.{ico,png,txt}',
+						// 'images/{,*/}*.webp',
+						'data/*',
+						'styles/fonts/{,*/}*.*',
+						'styles/fa/{,*/}*.*',
+						'styles/images/**/*.{gif,jpeg,jpg,png}',
+						'styles/*.css'
+					]// End src
+				}]// End copy:dist - files
+			}// End copy:dist
+		},// End copy task
 
+		// Auto save via FTP
+		'ftp-deploy': {
+			build: {
+				auth: {
+					host: 'sol.apisnetworks.com',
+					port: 21,
+					authKey: 'apisnetworks-key'
+				},
+				src: 'dist/',
+				dest: '/var/www/html/middleofjune/dist/',
+				exclusions: ['dist/.DS_Store', 'dist/Thumbs.db', 'dist/data/' ]
+			 }
+		}// End ftp-deploy task
 
-      // By default, your `index.html`'s <!-- Usemin block --> will take care
-      // of minification. These next options are pre-configured if you do not
-      // wish to use the Usemin blocks.
-      cssmin: {
-         dist: {
-             files: {
-                '<%= config.dist %>/styles/main.css': [
-                   '.tmp/styles/{,*/}*.css',
-                   '<%= config.app %>/styles/{,*/}*.css'
-                ]
-             }
-          }
-      },
+		// Runs JSHint (code quality analysis tool) against app JS files
+		// Rules defined in .jshintrc.
+		// THROWS ERRORS!!
+		//jshint: {
+		// options: {
+		//    jshintrc: '.jshintrc',
+		//    reporter: require('jshint-stylish')
+		// },
+		// assess: ['<%=cfg.app%>/js/**/*.js'],
+		// all: ['Gruntfile.js', '<%=cfg.app%>/js/**/*.js']
+		//},
 
-      // uglify: {
-      //    dist: {
-      //        files: {
-      //          '<%= config.dist %>/scripts/scripts.js': [
-      //             '<%= config.dist %>/scripts/scripts.js'
-      //          ]
-      //       }
-      //    }
-      // },
+		// Watches files for changes and runs tasks based on what's changed
+		//watch: require('./grunt-include/watch'),
+		//browserSync: require('./grunt-include/browserSync'),
 
-      // concat: {
-      //  dist: {}
-      // },
+		// // Reads HTML for usemin blocks to enable smart builds that automatically
+		// // concat, minify and revision files. Creates cfgurations in memory so
+		// // additional tasks can operate on them
+		// useminPrepare: {
+		//    options: {
+		//       dest: '<%= cfg.dst %>'
+		//    },
+		//    html: '<%= cfg.app %>/index.html'
+		// },
 
-      // Copies remaining files to places other tasks can use
-      copy: {
-         dist: {
-            files: [{
-               expand: true,
-               dot: true,
-               cwd: '<%= config.app %>',
-               dest: '<%= config.dist %>',
-               src: [
-                '{,*/}*.html',
-                'scripts/**/*.{js,php}',
-                // 'scripts/**/*.php',
-                '*.{ico,png,txt}',
-                // 'images/{,*/}*.webp',
-                'data/*',
-                'styles/fonts/{,*/}*.*',
-                'styles/fa/{,*/}*.*',
-                'styles/images/**/*.{gif,jpeg,jpg,png}',
-                'styles/*.css'
-               ]
-            }]
-         }
-      },
+		// // Performs rewrites based on rev and the useminPrepare cfguration
+		// usemin: {
+		//    options: {
+		//       assetsDirs: [
+		//          '<%= cfg.dst %>',
+		//          '<%= cfg.dst %>/images',
+		//          '<%= cfg.dst %>/styles'
+		//       ]
+		//    },
+		//    html: ['<%= cfg.dst %>/{,*/}*.html'],
+		//    css: ['<%= cfg.dst %>/styles/{,*/}*.css']
+		// },
 
-      // Run some tasks in parallel to speed up build process
-      // concurrent: {
-      //    server: [
-      //       'babel:dist',
-      //       'sass'
-      //    ],
-      //    test: [
-      //       'babel'
-      //    ],
-      //    dist: [
-      //       'babel',
-      //       'sass',
-      //       'imagemin',
-      //       'svgmin'
-      //    ]
-      // },
+		// // The following *-min tasks produce minified files in the dist folder
+		// imagemin: {
+		//    dist: {
+		//       files: [{
+		//          expand: true,
+		//          cwd: '<%= cfg.app %>/images',
+		//          src: '{,*/}*.{gif,jpeg,jpg,png}',
+		//          dest: '<%= cfg.dst %>/images'
+		//       }]
+		//    }
+		// },
 
-      // Auto save via FTP
-      'ftp-deploy': {
-         build: {
-            auth: {
-               host: 'sol.apisnetworks.com',
-               port: 21,
-               authKey: 'apisnetworks-key'
-            },
-            src: 'dist/',
-            dest: '/var/www/html/middleofjune/dist/',
-            exclusions: ['dist/.DS_Store', 'dist/Thumbs.db', 'dist/data/' ]
-         }
-      }
+		// concat: {
+		//  dist: {}
+		// },
 
-    });// End grunt initConfig
-
-    // --- GRUNT SERVE --------------------------------------------------------------------
-    //grunt.registerTask('serve', 'start the server and preview your app', function (target) {
-      // if (target === 'dist') {
-      //    return grunt.task.run(['build', 'browserSync:dist']);
-      // }
-
-      // grunt.task.run([
-      //    'clean:server',
-      //    // 'wiredep',     // turn on only when bower-components is reinstated
-      //    'concurrent:server',
-      //    'postcss',
-      //    'browserSync:livereload',
-      //    // 'ftp-deploy',
-      //    'watch'
-      // ]);
-    //});// End serve task
-
-    // grunt.registerTask('server', function (target) {
-    //   grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    //   grunt.task.run([target ? ('serve:' + target) : 'serve']);
-    // });// End register task :: server
-
-    grunt.registerTask('test', function (target) {
-      if (target !== 'watch') {
-         grunt.task.run([
-            'clean:server',
-            // 'concurrent:test',
-            'postcss'
-         ]);
-      }// End if target !== watch
-
-    });// End task "test"
-
-    //   grunt.task.run([
-    //      'browserSync:test',
-    //      'mocha'
-    //   ]);
-    // });// End register task :: test
-
-    // This is the default Grunt task if you simply run "grunt" in project dir
-    grunt.registerTask('build', [
-        'clean:dist',
-        'postcss',
-        'copy:dist',
-        'ftp-deploy'
-    //   // 'wiredep',     // turn on only when bower-components is reinstated
-    //   // 'useminPrepare',  // throws processing a template error ('test' is undefined)
-    //   'concurrent:dist',
-        
-    //   // 'concat',
-    //   'cssmin',
-    //   'uglify',
-        
-        // 'filerev'
-    //   'usemin',
-    //   'htmlmin',
-        
-    ]);// End register task :: build
-
-    grunt.registerTask('default', [
-        //'newer:eslint',
-        'build',
-        'test'
-    ]);// End register task :: default
+		
 
 
-    // Launch Browser-sync & watch files
-    //  grunt.registerTask('watch', ['bs-init', 'watch']);
-    
-    // Once installed with "npm install <TASK_NAME> --save-dev",
-    // we have to enable them!
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-postcss');
-    grunt.loadNpmTasks('grunt-ftp-deploy');
+		// Run some tasks in parallel to speed up build process
+		// concurrent: {
+		//    server: [
+		//       'babel:dist',
+		//       'sass'
+		//    ],
+		//    test: [
+		//       'babel'
+		//    ],
+		//    dist: [
+		//       'babel',
+		//       'sass',
+		//       'imagemin',
+		//       'svgmin'
+		//    ]
+		// },
+
+	});// End grunt initcfg
+
+	// 
+	// --- GRUNT REGISTER TASKS  -----------------------------------------------
+	//
+	grunt.registerTask('test', function (target) {
+		if (target !== 'watch') {
+			grunt.task.run([
+				'clean:server',
+				// 'concurrent:test',
+				'postcss'
+			]);
+		}// End if target !== watch
+	});// End task "test"
+
+	// This is the default Grunt task if you simply run "grunt" in project dir
+	grunt.registerTask('build', [
+		'clean:dist',
+		'postcss',
+		'copy:dist',
+		'uglify'
+	//   // 'wiredep',     // turn on only when bower-components is reinstated
+	//   // 'useminPrepare',  // throws processing a template error ('test' is undefined)
+	//   'concurrent:dist',
+		
+	//   // 'concat',
+	//   'cssmin',
+	//   'uglify',
+		
+		// 'filerev'
+	//   'usemin',
+	//   'htmlmin',
+		
+	]);// End register task :: build
+
+	grunt.registerTask('default', [
+		//'newer:eslint',
+		'build',
+		'test'
+	]);// End register task :: default
+
+	grunt.registerTask('production', [
+		'clean:dist',
+		'postcss',
+		'copy:dist',
+		'uglify',
+		'ftp-deploy'
+	]);
+
+	
+	//grunt.registerTask('serve', 'start the server and preview your app', function (target) {
+	  // if (target === 'dist') {
+	  //    return grunt.task.run(['build', 'browserSync:dist']);
+	  // }
+
+	  // grunt.task.run([
+	  //    'clean:server',
+	  //    // 'wiredep',     // turn on only when bower-components is reinstated
+	  //    'concurrent:server',
+	  //    'postcss',
+	  //    'browserSync:livereload',
+	  //    // 'ftp-deploy',
+	  //    'watch'
+	  // ]);
+	//});// End serve task
+
+	// grunt.registerTask('server', function (target) {
+	//   grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+	//   grunt.task.run([target ? ('serve:' + target) : 'serve']);
+	// });// End register task :: server
+
+	
+
+	//   grunt.task.run([
+	//      'browserSync:test',
+	//      'mocha'
+	//   ]);
+	// });// End register task :: test
+
+
+	// Launch Browser-sync & watch files
+	//  grunt.registerTask('watch', ['bs-init', 'watch']);
+	
+
+
+	//
+	// LOAD NPM TASKS
+	//
+	// Once installed with "npm install <TASK_NAME> --save-dev",
+	// we have to enable them!
+	//
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-ftp-deploy');
+
 };// End module.exports
