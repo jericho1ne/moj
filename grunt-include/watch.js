3fd63de5522924ba46640a181ba7011fc3678046
Mihai Peteu <jericho1ne@yahoo.com>
2016-02-03 00:49:11 -0800

    css related

--- grunt-include/watch.js
+++ grunt-include/watch.js
@@ -21,7 +21,7 @@ module.exports = {
 	sass: {
 		options: {livereload: true},
 		files: ['<%= cfg.src %>/styles/{,*/}*.{scss,sass}'],
-		tasks: ['clean:maincss', 'sass', 'postcss', 'cssmin']
+		tasks: ['clean:maincss', 'sass', 'postcss', 'cssmin','filerev']
 	},
 	corefiles: {
 		options: {livereload: true},
