195eaad5523ef38e1538b94fed62bf36c29c1c8c
Mihai Peteu <jericho1ne@yahoo.com>
2016-02-03 01:06:16 -0800

    exclude data dir in ftpush

--- src/scripts/api/getEvents.php
+++ src/scripts/api/getEvents.php
@@ -38,34 +38,3 @@ else {
 // Save to CSV (if necessary)
 // $eventParser->saveVenuesCsv("../../data/venues.csv");
 // $eventParser->saveEventsCsv("../../data/events.csv");
-
-
-// ============================ HTML OUTPUT SECTION ===========================
-// Comment me out and use json_encode instead
-// $prev_month = "";
-// $prev_wkday = "";
-
-// $php_events_array = $eventParser->getEvents();
-// echo '<span class="small">';
-// foreach ($php_events_array as $show) {
-// 	//$nice_date = date("D M d", strtotime($show["date"]));
-// 	$weekday 	= date("l", strtotime($show["fmt_date"]));
-// 	$month 		= date("F", strtotime($show["fmt_date"]));
-
-// 	// Monthly header
-// 	if ($prev_month!=$month ) {											
-// 		echo "<hr><h2>".$month." ".$show["year"]."</h2>";
-// 	}	
-
-// 	// Day of week header
-// 	if ($prev_wkday!=$weekday) {								
-// 		echo "<h3>".$show["nice_date"]."</h3>";
-// 	}	
-// 		echo ' <a href="'.$show['url'].'">'.$show['artist'].'</a> '.$show['venue'].'<br>';
-// 	$prev_month	= $month;
-// 	$prev_wkday = $weekday;
-// }// End foreach
-// echo '</span>';
-//============================================================================ 
-
-?>
