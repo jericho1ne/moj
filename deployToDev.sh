5622eebb614ba1360149a0a5e76a582bca832030
Mihai Peteu <jericho1ne@yahoo.com>
2016-10-12 20:12:32 -0700

    deploy shell script

---
+++ deployToDev.sh
@@ -0,0 +1,3 @@
+rsync app/index.html -v -i mpeteu#uzabl.com@sol.apisnetworks.com:/home/mpeteu/mainwebsite_html/middleofjune/dev
+rsync app/scripts/*.js -v -i mpeteu#uzabl.com@sol.apisnetworks.com:/home/mpeteu/mainwebsite_html/middleofjune/dev/scripts
+rsync app/scripts/ui/*.js -v -i mpeteu#uzabl.com@sol.apisnetworks.com:/home/mpeteu/mainwebsite_html/middleofjune/dev/scripts/ui
